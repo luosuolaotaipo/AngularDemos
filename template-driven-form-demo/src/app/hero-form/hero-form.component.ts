679f89b2a56a9ba443d27089ff53d5346a9ab9e3
huimin.xu <huimin.xu@accenture.com>
2018-04-21 18:14:44 +0800

    form drive by modal template

---
+++ template-driven-form-demo/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  public modelMessage:string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
